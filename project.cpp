0d62a15986efe35456341168bda93aa4f499ace7
Piyush Srivastava <piyush061996@gmail.com>
2017-04-03 21:50:57 +0000

    Added new task function and ToDo class

--- project.cpp
+++ project.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <fstream>
+#include <sqlite>
 
 using namespace std;
 
@@ -16,13 +17,21 @@ class Habit{
         time = t;
     }
     
+};
+
+class ToDo:public Habit{
+    public:
+    ToDo(char *a ,int t):task(a),time(t){}
+    
+    
+    
 }
 
 void new_task(){
     Habit h1;
-    Daily d1;
+    ToDo d1;
     cout<<"Select the type of task:\n ";
-    cout<<"1.Habit\n 2.Dailies\n";
+    cout<<"1.Habit\n 2.To-Do\n";
     int ch;
     char* s[100];
     cin>>ch;
@@ -31,10 +40,28 @@ void new_task(){
     {
         case 1: cout<<"Enter the task: \n";
                 cin>>s;
+                cout<<"Enter the deadline in hours:\n";
+                cin>>time;
                 h1(s,time);
+                break;
+        
+        case 2: cout<<"Enter the task:\n";
+                cin>>s;
+                cout<<"Enter the deadline in hours:\n";
+                cin>>time;
+                d1(s,time);
+                break;
+        
+        default: cout<<"Invalid option\n";
+        
+                
                 
                 
     }
+    
+}
+
+void sql_(){
     
 }
 
@@ -42,21 +69,24 @@ void new_task(){
 int main()
 {
     cout<<"Welcome to the Habilist\n";
+    while(1){
     cout<<"Select your choice\n: ";
-    cout<<"1.Enter a new task\n 2.Edit a task\n 3.Delete a task\n";
+    cout<<"1.Enter a new task\n 2.Edit a task\n 3.Delete a task \n4.Display all tasks \n5.Exit\n";
     int ch;
     switch(ch)
     {
-        case 1: void new_task();
+        case 1: void new_task();break;
                 
-        case 2: void edit_task();
+        case 2: void edit_task();break;
         
-        case 3: void delete_task();
+        case 3: void delete_task();break:
+        
+        case 4: exit(0);
         
         default: exit(0);
     }
     
     
-    
+    }
     
 }
